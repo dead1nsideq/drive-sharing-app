efa5cce7062de30b75b893e87dc703cde97f7edb
Kostiantyn Hromovyi <ydydydydydyyd@gmail.com>
2024-01-15 14:10:17 +0200

    add trip store

--- frontend/src/stores/trip.js
+++ frontend/src/stores/trip.js
@@ -1,3 +1,26 @@
 // TODO сохранять поездку что бы не терялась в процессе,
 //  возможно делать это в locationStore,смотреть
 //  если ли сейчас активная поезда у пользователя или у водителя
+
+
+import {defineStore} from "pinia";
+import {reactive, ref} from "vue";
+
+export const useTripStore = defineStore('trip', () => {
+    const id = ref(null);
+    const user_id = ref(null);
+
+    const origin = reactive({
+        lat: null,
+        lng: null
+    })
+
+    const destination = reactive({
+        lat: null,
+        lng: null
+    })
+
+    const destination_name = ref('')
+
+    return { id, user_id, origin, destination, destination_name }
+})
